4c5c4be13f6e101fe9778fa444dceff10d189fae
Etienne <etienne.secondini@revolve.team>
2023-02-21 19:35:28 +0100

    fix: permission not checked

--- fitnessapp/lib/screens/loading.dart
+++ fitnessapp/lib/screens/loading.dart
@@ -11,13 +11,32 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   void getLocationIfServiceEnabled() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'latitude': position.latitude,
       'longitude': position.longitude,
     });
-    print('ici');
   }
 
   @override
